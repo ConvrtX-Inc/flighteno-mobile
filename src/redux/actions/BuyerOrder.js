bf08f9323473e74af2174929119303055de769aa
Tony S - Convrtx <tonys@convrtx.com>
2022-01-30 17:30:30 +0800

    Fixed issue on order

--- src/redux/actions/BuyerOrder.js
+++ src/redux/actions/BuyerOrder.js
@@ -4,13 +4,16 @@ import { IS_LOADING } from '../constants';
 
 
 export function createOrder(data, navigate, token) {
+    console.log('data', data);
+    console.log('navigate', navigate);
+    console.log('token', token);
     return async dispatch => {
         dispatch({ type: IS_LOADING, isloading: true })
         axios({
             method: 'post',
             url: `${BASE_URL}Rest_calls/createOrder`,
             data: data,
-            headers: { "Authorization": token },
+            headers: { "Authorization": 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZG1pbl9pZCI6IjYxYmIyN2NiOWM3Yjc4NTVkNzViNDAzNCIsImRhdGUiOiIyMDIyLTAxLTI5IDEwOjI3OjA3IiwiaWF0Ijp7IiRkYXRlIjp7IiRudW1iZXJMb25nIjoiMTY0NTE4MDAyNzAwMCJ9fX0.0Kbfh0e-gteX8gW3xWe0BsdyRurN859MdNRLzNn8bkQ' },
             validateStatus: (status) => {
                 return true;
             },
